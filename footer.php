2ae399b0a9af60421a158324522141a56f4dca50
Yura Holudeev <yustasvega@users.noreply.github.com>
2023-09-15 22:15:27 +0700

    123 enter

--- footer.php
+++ footer.php
@@ -16,6 +16,7 @@
 					'items_wrap'      => '<h4>Информация</h4><ul id="%1$s" class="%2$s">%3$s</ul>',
 					'depth'           => 2,
 				]) ?>
+
 			</div>
 
 			<div class="col-lg-2 col-sm-6 col-md-6">
@@ -30,15 +31,16 @@
 				]) ?>
 			</div>
 			<div class="col-lg-3 col-sm-6 col-md-6">
-			<?php if (!dynamic_sidebar('sidebar-footer-contacts')) :  dynamic_sidebar('sidebar-footer-contacts');
-			endif; ?>
+				<?php if (!dynamic_sidebar('sidebar-footer-contacts')) :  dynamic_sidebar('sidebar-footer-contacts');
+				endif; ?>
 			</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-12 text-center">
-			<div class="footer-copy">© <?php echo date( ' Y ' ); echo get_bloginfo('name')?> inc. Все права защищены.</div>
 		</div>
-	</div>
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<div class="footer-copy">© <?php echo date(' Y ');
+																		echo get_bloginfo('name') ?> inc. Все права защищены.</div>
+			</div>
+		</div>
 	</div>
 </section>
 <!--  FOOTER AREA END  -->
@@ -51,6 +53,6 @@
 
 </script>
 <?php wp_footer(); ?>
-				</body>
+</body>
 
-				</html>
+</html>
--- template-parts/content-service.php
+++ template-parts/content-service.php
@@ -1,21 +1,20 @@
 <!--  SERVICE PARTNER START  -->
-<section id="service-head" class="<?php echo $args['class'] ?>">
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-lg-8 col-sm-12 m-auto">
-				<div class="heading text-white text-center">
-					<h4 class="section-title text-white">
-						<?php if (!empty($args['custom+title'])) {
-							echo $args['custom_title'];
-						} else
-							echo 'Список услуг' ?></h4>
-					<p>
-						Это означает, что мы сможем выполнить любую цифровую задачу: <br />
-						видео, маркетинг, реклама, разработка или дизайн.
-					</p>
-				</div>
-			</div>
+<section id="service-head" class="<?php echo $args['class'] ?>" <div class=" container"">
+	<div class=" row justify-content-center">
+	<div class="col-lg-8 col-sm-12 m-auto">
+		<div class="heading text-white text-center">
+			<h4 class="section-title text-white"></h4>
+			<?php if (!empty($args['custom+title'])) {
+				echo $args['custom_title'];
+			} else
+				echo 'Список услуг' ?></h4>
+			<p>
+				Это означает, что мы сможем выполнить любую цифровую задачу: <br />
+				видео, маркетинг, реклама, разработка или дизайн.
+			</p>
 		</div>
+	</div>
+	</div>
 	</div>
 </section>
 <!--  SERVICE PARTNER END  -->
@@ -35,24 +34,24 @@
 				while ($query->have_posts()) {
 					$query->the_post();
 			?>
-					<div class="col-lg-4 col-sm-6 col-md-6">
-						<div class="service-box">
-							<div class="service-img-icon">
-								<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="service-icon" class="img-fluid" />
-							</div>
-							<div class="service-inner">
-								<h4><?php the_title(); ?></h4>
-								<p>
-									<?php the_excerpt(); ?>
-								</p>
-							</div>
-						</div>
+			<div class="col-lg-4 col-sm-6 col-md-6">
+				<div class="service-box">
+					<div class="service-img-icon">
+						<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="service-icon" class="img-fluid" />
 					</div>
-				<?php
+					<div class="service-inner">
+						<h4><?php the_title(); ?></h4>
+						<p>
+							<?php the_excerpt(); ?>
+						</p>
+					</div>
+				</div>
+			</div>
+			<?php
 				}
 			} else {
 				?>
-				<p>Услуг пока нет</p>
+			<p>Услуг пока нет</p>
 			<?php
 			}
 
